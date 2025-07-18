1f25de8abac39b6561e6033504d59c24b30de097
Matteo <sanchez.matteopro@gmail.com>
2025-07-18 14:31:28 +0200

    Add files via upload

---
+++ HydroLine/utils/error.py
@@ -0,0 +1,16 @@
+# utils/error.py
+
+
+from qgis.PyQt.QtWidgets import QAction, QMessageBox
+
+def afficher_message_epsg():
+    """
+    Affiche une boîte de dialogue avertissant de la nécessité de mettre le projet en EPSG:2154.
+    """
+    QMessageBox.warning(
+        None,
+        "Attention projet QGIS",
+        ("Erreur de conversion des coordonnées.\n"
+         "Veuillez assurer que votre projet QGIS est configuré sur EPSG:2154 pour éviter "
+         "les erreurs de transformation des points.")
+    )
