1b59bd84f70e77d927bc0fd78e6b724b6965f08c
Matteo <sanchez.matteopro@gmail.com>
2025-07-18 14:52:04 +0200

    Add files via upload

--- HydroLine/utils/error.py
+++ HydroLine/utils/error.py
@@ -14,3 +14,11 @@ def afficher_message_epsg():
          "Veuillez assurer que votre projet QGIS est configuré sur EPSG:2154 pour éviter "
          "les erreurs de transformation des points.")
     )
+
+def afficher_changer_vers_mode_convexe():
+    QMessageBox.warning(
+        None,
+        "Attention option instable",
+        ("Le mode convexe est fiable. Le mode concave \n"
+         "est en développement !")
+    )
